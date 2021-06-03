dfdda546daccb713bb2958d753e2801f0acd68a9
Carina Sweet <carina.sweet06@gmail.com>
2021-06-03 09:06:43 -0600

    Add tests for spacecraft class

---
+++ spec/spacecraft_spec.rb
@@ -0,0 +1,17 @@
+require './lib/spacecraft'
+
+RSpec.describe Spacecraft do
+
+  it 'exists' do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus).to be_an_instance_of(Spacecraft)
+  end
+
+  it 'has attributes' do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus.name).to eq('Daedalus')
+    expect(daedalus.fuel).to eq(400)
+  end
+end

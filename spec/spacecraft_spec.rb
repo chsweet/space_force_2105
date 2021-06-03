34a023f55c580d04b139b77eef6bd863aa2b9d30
Carina Sweet <carina.sweet06@gmail.com>
2021-06-03 09:20:29 -0600

    Add additional spacecraft test for method

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -13,5 +13,16 @@ RSpec.describe Spacecraft do
 
     expect(daedalus.name).to eq('Daedalus')
     expect(daedalus.fuel).to eq(400)
+    expect(daedalus.requirements).to eq([])
   end
+
+  it 'adds spacecraft requirements' do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    daedalus.add_requirement({astrophysics: 6})
+    daedalus.add_requirement({quantum_mechanics: 3})
+
+    expect(daedalus.requirements).to eq([[{astrophysics: 6}, {quantum_mechanics: 3}]])
+  end
+
 end
